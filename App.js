7ba8a81c0e85ff53473eb8d77f7de20ffd3124b1
hardik-kh <hardik.eldeco@gmail.com>
2021-06-25 17:36:45 +0530

    Navigation done , Data shared btw screens , Location Supported

--- App.js
+++ App.js
@@ -3,26 +3,19 @@ import { StyleSheet, View,ScrollView ,FlatList} from 'react-native';
 import Search from './screens/Search';
 import Home from './screens/Home';
 import Day from './screens/Day';
+import Navigator from './routes/homeStack';
+
+
 
 export default function App() {
 
+  //console.disableYellowBox = true;
+  //LogBox.ignoreAllLogs(disable);
+
     return (
-
-        <View style={styles.container}> 
-          <Day />
-        </View>
-
-  
-
-
-
-
-
-    
-
- 
-    
-
+      <Navigator 
+      navigationOptions={{headerShown: false}}/>
+   
   );
 }
 
--- screens/Home.js
+++ screens/Home.js
@@ -17,7 +17,6 @@ export default function Home(){
         .then(info =>{
             console.log(info.data.data[0])
         })
- //   } 
     
     return(
         <View>
